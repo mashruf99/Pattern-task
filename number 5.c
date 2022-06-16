cc8f779277ff560f836536f9b5333551e8e1f42b
mashruf99 <105199738+mashruf99@users.noreply.github.com>
2022-06-16 19:24:35 +0600

    5

---
+++ number 5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() {
+    int a,i,j;
+    scanf("%d",&a);
+
+    for(i=1 ; i<=a; i++) {
+        for(j=0 ; j<i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(i=a-1 ; i>=1; i--) {
+        for(j=0 ; j<i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}
